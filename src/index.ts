28dd0847a9448d420dfa53123b40a8f2fffa58f5
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-10 21:09:39 +0530

    :sparkles: Use dotenv module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import chalk from 'chalk';
 import { DatabaseService } from './Services/DatabaseService';
 
+require('dotenv').config();
+
 const app = async () => {
   await DatabaseService.getInstance().testDatabaseConnection();
   console.log(chalk.green(chalk.bold('Success ')) + 'Connected to Database');
